39ef68aa330c36cc3896cb9f3b76170ebc2d623b
adkamin <49719000+adkamin@users.noreply.github.com>
2021-09-07 13:20:09 +0200

    Add files via upload

---
+++ Say.hs
@@ -0,0 +1,33 @@
+module Say where
+
+say :: Integer -> String
+say  0 = "zero"
+say  1 = "one"
+say  2 = "two"
+say  3 = "three"
+say  4 = "four"
+say  5 = "five"
+say  6 = "six"
+say  7 = "seven"
+say  8 = "eight"
+say  9 = "nine"
+
+say 10 = "ten"
+say 11 = "eleven"
+say 12 = "twelve"
+say 13 = "thirteen"
+say 14 = "fourteen"
+say 15 = "fifteen"
+say 16 = "sixteen"
+say 17 = "seventeen"
+say 18 = "eighteen"
+say 19 = "nineteen"
+
+say 20 = "twenty"
+say 30 = "thirty"
+say 40 = "forty"
+say 50 = "fifty"
+say 60 = "sixty"
+say 70 = "seventy"
+say 80 = "eighty"
+say 90 = "ninety"
