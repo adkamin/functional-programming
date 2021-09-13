558fd4c58d011061ca63843b7715814596089732
adkamin <minichovaa@gmail.com>
2021-09-13 18:48:16 +0200

    Char.hs

--- week2/Char.hs
+++ week2/Char.hs
@@ -2,13 +2,30 @@ module Char where
 
 import Data.Char
 
---(~~) :: String -> String -> Bool
+(~~) :: String -> String -> Bool
+(~~) (x:xs) (y:ys) = toLower x == toLower y && (~~) xs ys
+(~~) [] []         = True
+(~~) [] _          = False
+(~~) _ []          = False
 
---reverseCase :: String -> String
+reverseCase :: String -> String
+reverseCase (x:xs) = if isLower x then toUpper x : reverseCase xs else toLower x : reverseCase xs
+reverseCase []     = ""
 
---shift :: Int -> Char -> Char
+shift :: Int -> Char -> Char
+shift n c = chr ((ord c - 64 + n) `mod` 26 + 64)
 
---caesar :: Int -> String -> String
+caesar :: Int -> String -> String
+caesar n (s:sx)
+    | isAscii s && isLower s = shift n (toUpper s) : caesar n sx
+    | isAscii s && isUpper s = shift n s : caesar n sx
+    | otherwise              = s : caesar n sx
+caesar n []     = ""
 
 msg :: String
 msg = "ADMNO D HPNO NKMDIFGZ TJP RDOC AVDMT YPNO"
+-- decoded to: "FIRST I MUST SPRINKLE YOU WITH FAIRY DUST"
+
+decode :: Int -> String -> String
+decode 0 s = ""
+decode n s = caesar n s ++ "\n" ++ decode (n-1) s
