453847ea1e86a5298efd74e354308b625296c3ab
adkamin <minichovaa@gmail.com>
2021-09-13 15:50:13 +0200

    Pow2.hs

--- week2/Pow2.hs
+++ week2/Pow2.hs
@@ -1,9 +1,6 @@
 module Pow2 where
 
-pow2 :: Integer -> Integer
+pow2 :: (Ord n, Num n, Num a) => n -> a
 pow2 0 = 1
--- pow2 n = ... recursive case
+pow2 n = 2 * pow2 (n-1)
 
-
---after finishing the function, change the type to: 
---pow2 :: (Ord n, Num n, Num a) => n -> a
