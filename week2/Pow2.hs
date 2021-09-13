0fd5b2620bec04634f59e86fc3ddde3dc0a110f4
adkamin <minichovaa@gmail.com>
2021-09-13 17:09:07 +0200

    Polytype.hs finished

--- week2/Pow2.hs
+++ week2/Pow2.hs
@@ -2,6 +2,7 @@ module Pow2 where
 
 import Numeric
 
+-- Part 1 & 2
 pow2 :: (Ord n, Num n, Num a) => n -> a
 pow2 0 = 1
 pow2 n = 2 * pow2 (n-1)
@@ -9,6 +10,9 @@ pow2 n = 2 * pow2 (n-1)
 showFullPrecision :: Double -> String
 showFullPrecision x = showFFloat Nothing x ""
 
--- (first bit is sign, therefore instead of 32 and 64, there is a space for 31 and 63 bits to represent numbers
--- Int has an upper bound of 2^63
--- Integer is unlimited, so this depends on a specific hardware
+-- Part 3
+{-
+(The first bit is sign, therefore instead of 32 and 64, there is a space for 31 and 63 bits to represent numbers.
+Int has an upper bound of 2^63.
+Integer is unlimited, so this depends on a specific hardware.
+-}
