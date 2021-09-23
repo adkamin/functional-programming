a6d7132cd237719ebbaa8759fa31d4cb26d430f5
adkamin <minichovaa@gmail.com>
2021-09-23 18:20:22 +0200

    Finish Lego.hs

--- week3/Lego.hs
+++ week3/Lego.hs
@@ -5,13 +5,15 @@ import Data.Tuple
 
 removeAt :: Int -> [a] -> [a]
 removeAt n xs = [x | (x,i) <- zip xs [1..], i /= n]
---removeAt n xs = take (n-1) xs ++ reverse (take n (reverse xs))
+-- Alternative: 
+-- removeAt n xs = take (n-1) xs ++ reverse (take n (reverse xs))
 
 sortWithPos :: (Ord a) => [a] -> [(a,Int)]
 sortWithPos xs = sort [(x,i) | (x,i) <- zip xs [0..]]
 
---sortedPos :: (Ord a) => [a] -> [(a,Int)]
-sortedPos xs = [(s,i) | s <- xs, (x,i) <- l, x == s]
+sortedPos :: (Ord a) => [a] -> [(a,Int)]
+sortedPos xs = removeDups [(s,i) | s <- xs, (x,i) <- l, x == s]
     where l = (zip (sort xs) [0..])
 
-removeDups xs = map filter () xs
+removeDups :: (Eq a) => [a] -> [a]
+removeDups xs = [x | (i,x) <- zip [0..] xs, notElem x (take i xs)]
