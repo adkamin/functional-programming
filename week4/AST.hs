495f6d83a03767f44d12462c54a42879fca7a21a
adkamin <minichovaa@gmail.com>
2021-09-30 19:41:42 +0200

    AST.hs progress

--- week4/AST.hs
+++ week4/AST.hs
@@ -1,11 +1,25 @@
 module AST where
 
---start with either:
---  data Expr = Lit Integer | Add Expr Expr | Mul Expr Expr | ...
---or:
---  data Expr = Lit Integer | Expr :+: Expr | Expr :*: Expr | ...
---  infixl 6 :+:
---  infixl 7 :*:
+data Expr = 
+    VarX          |
+    Lit   Integer |
+    Expr :+: Expr | 
+    Expr :-: Expr | 
+    Expr :*: Expr | 
+    Expr :/: Expr 
+
+infixl 6 :+:
+infixl 7 :-:
+infixl 8 :*:
+infixl 9 :/:
+
+eval :: (Fractional a) => Expr -> a -> a
+eval (Lit i) _    = fromIntegral i
+eval (VarX)  n    = n
+eval (e1 :+: e2) n = eval e1 n + eval e2 n
+eval (e1 :-: e2) n = eval e1 n - eval e2 n
+eval (e1 :*: e2) n = eval e1 n * eval e2 n
+eval (e1 :/: e2) n = eval e1 n / eval e2 n
 
 
 --eval :: (Fractional a) => Expr -> a -> Maybe a
