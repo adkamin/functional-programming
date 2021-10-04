3baa1972868e4c72641d22cc472163a5030e2de7
adkamin <minichovaa@gmail.com>
2021-10-04 23:43:58 +0200

    Folders.hs

--- week5/Folders.hs
+++ week5/Folders.hs
@@ -8,13 +8,26 @@ and = foldr (&&) True
 or :: [Bool] -> Bool
 or = foldr (||) False
 
---elem :: (Eq a) => a -> [a] -> Bool
+elem :: (Eq a) => a -> [a] -> Bool
+elem el = foldr (\x xs -> el == x || xs) False
 
---maximum :: (Ord a) => [a] -> a
+maximum :: (Ord a) => [a] -> a
+maximum [] = error "cannot find maximum of empty list"
+maximum xs = foldr (max) (last xs) xs
 
---fromList :: (Ord a) => [a] -> Tree a
+fromList :: (Ord a) => [a] -> Tree a
+fromList = foldr (\x -> insert x) Leaf
 
---fromBits :: [Integer] -> Integer
+fromBits :: [Integer] -> Integer
+fromBits = foldl (\acc x -> x + 2 * acc) 0
+
+{-
+Works, thanks to the pattern:
+[1,0,1,0,1,0] = 2×(2×(2×(2×(2×1+0)+1)+0)+1)+0
+              = 32×1 + 16×0 + 8×1 + 4×0 + 2×1 + 1×0
+              = 42
+-}
+
 
 {- -------------------------------------------------------------------}
 
