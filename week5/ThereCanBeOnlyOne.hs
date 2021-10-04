fda5469defbe9a5f684e93ecf80ccd87bfeb7f85
adkamin <minichovaa@gmail.com>
2021-10-04 22:27:58 +0200

    ThereCanOnlyBeOne.hs

--- week5/ThereCanBeOnlyOne.hs
+++ week5/ThereCanBeOnlyOne.hs
@@ -1,5 +1,16 @@
 module ThereCanBeOnlyOne where
 
--- onlyElem :: (Eq a) => a -> [a] -> Bool
+onlyElem :: (Eq a) => a -> [a] -> Bool
+onlyElem el []     = False
+onlyElem el (x:xs)
+    | el == x = True && not (onlyElem el xs)
+    | el /= x = onlyElem el xs
 
--- onlyOnce :: (a -> Bool) -> [a] -> Bool
+onlyOnce :: (a -> Bool) -> [a] -> Bool
+onlyOnce p []     = False 
+onlyOnce p (x:xs)
+    | p x == True  = True && not (onlyOnce p xs)
+    | p x == False = onlyOnce p xs 
+
+onlyElem' :: (Eq a) => a -> [a] -> Bool
+onlyElem' el xs = onlyOnce (==el) xs
