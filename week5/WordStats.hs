74bbba181ccf9a3bf0ed8f3a15567d01c079223b
adkamin <minichovaa@gmail.com>
2021-10-10 17:42:02 +0200

    Finish anagrams and wordLengthFrequency

--- week5/WordStats.hs
+++ week5/WordStats.hs
@@ -9,21 +9,14 @@ wordFrequency  = map (\x -> (head x, length x)) . group . sort . words
 mostFrequencyOfLength :: Int -> String -> [(String, Int)]
 mostFrequencyOfLength n = reverse . (filter (\(s,i) -> length s >= n)) . wordFrequency 
 
-wordLengthFrequency :: String -> [[(Int, Int)]]
-wordLengthFrequency = sumLengths . groupLengths . getLengths
-  where 
-    sumLengths   = map collapse
-    groupLengths = groupBy (\t1 t2 -> fst t1 == fst t2) . sortOn fst
-    getLengths   = map (\(s,i) -> (length s, i)) . mostFrequencyOfLength 0 
+wordLengthFrequency :: String -> [(Int, Int)]
+wordLengthFrequency = map (\x -> (head x, length x)) . group . sort . map length . words
 
--- don't know yet how to do this with foldr or map or something similar
-collapse [(a,b)]            = [(a,b)]
-collapse ((a,b):(c,d):rest) = collapse ((a,(b+d)):rest)
--- collapse = foldr (\((a,b):(c,d):rest) -> ((a,(b+d)):rest)) []
-
---missess removing duplicates
 anagrams :: String -> [[String]]
-anagrams = filter (\l -> length l > 1) . groupBy (\t1 t2 -> length (union t1 t2) == length t1) . reverse . (sortBy (compare `on` length)) . words
+anagrams = filterAnagrams . groupByEqualEl
+  where 
+    filterAnagrams = filter (\l -> not (all (== head l) l))
+    groupByEqualEl = groupBy (\t1 t2 -> length (union t1 t2) == length t1) . reverse . (sortBy (compare `on` length)) . words
 
 main :: IO ()
 main = onStdin $ wordFrequency  -- change this to run a different function
