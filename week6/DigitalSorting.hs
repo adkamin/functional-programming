31f4821d5f381a13445c9254365c4967ed8aa13a
adkamin <minichovaa@gmail.com>
2021-10-15 12:26:31 +0200

    Progress DigitalSorting.hs

--- week6/DigitalSorting.hs
+++ week6/DigitalSorting.hs
@@ -15,13 +15,29 @@ digitalSortOn f = concat . rank . map (\x->(f x, x))
 digitalSort :: (Rankable key) => [key] -> [key]
 digitalSort = digitalSortOn id
 
+--1
 genericRank :: (Ord key) => [(key,a)] -> [[a]]
 genericRank = map (map snd) . groupBy (on (==)fst) . sortOn fst
 
---instance Rankable Int where ... etc.
+--2
+instance Rankable Int where
+  rank = genericRank
 
---instance Rankable Bool where ...
+instance Rankable Integer where
+  rank = genericRank
 
---instance (Rankable key1, Rankable key2) => Rankable (key1,key2) where
+instance Rankable Char where
+  rank = genericRank
 
---etc.
+--3
+instance Rankable Bool where
+  rank bs = [[v | (k,v) <- bs, k],[v | (k,v) <- bs, not k]]
+-- To do this in one pass, we can use foldr with a helper function which appends the element to the relevant list based on its value
+
+--4
+-- rank :: (Rankable key1, Rankable key2) ⇒ [((key1,key2),a)] → [[a]]
+-- assoc :: ((k1,k2),a) → (k1,(k2,a))
+
+-- instance (Rankable key1, Rankable key2) => Rankable (key1,key2) where
+--   rank ts = 
+
