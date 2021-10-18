8e6637ed8870ca8bf11733c01c617b47c931ee2c
adkamin <minichovaa@gmail.com>
2021-10-18 17:34:49 +0200

    PolyTypes.hs

--- week7/PolyTypes.hs
+++ week7/PolyTypes.hs
@@ -1,10 +1,17 @@
 module PolyTypes where
 
+justs :: [Maybe Char] -> [Char]
 justs xs           = [ x | Just x <- xs, x /= ' ' ]
 
+orderPairs :: (Ord a) => [(a,a)] -> [(a,a)] 
 orderPairs xs      = map (\(x,y)->(min x y, max x y)) xs
 
+unmaybe :: Maybe (Maybe a) -> Maybe a
 unmaybe (Just x)   = x
 unmaybe Nothing    = Nothing
 
-accumulate f st    = let (x,st') = f st in x : accumulate f st'
+accumulate :: (a -> (b, a)) -> a -> [b]
+accumulate f st    = let (x,st') = f st in x : accumulate f st' 
+
+-- accumulate f st = x : accumulate f st'
+--     where (x,st') = f st
