c10049121a9ec70974d9d9bd20fb31a5644fa515
adkamin <minichovaa@gmail.com>
2021-10-18 17:55:50 +0200

    FindDefs.hs

--- week7/FindDefs.hs
+++ week7/FindDefs.hs
@@ -1,9 +1,19 @@
 module FindDefs where
 
---mapFilter :: (a -> Maybe b) -> [a] -> [b]
+import Data.Maybe
+import Data.Monoid
 
---lift      :: (a -> b -> Maybe c) -> (Maybe a -> Maybe b -> Maybe c)
+mapFilter :: (a -> Maybe b) -> [a] -> [b]
+mapFilter f = map fromJust . map f
 
---compute   :: (Monoid n) => (a -> n) -> [a] -> n
+lift :: (a -> b -> Maybe c) -> (Maybe a -> Maybe b -> Maybe c)
+lift f = g
+    where g Nothing _         = Nothing
+          g _ Nothing         = Nothing
+          g (Just x) (Just y) = f x y
 
---fuse      :: (a -> b -> c) -> (a -> b) -> a -> c
+compute :: (Monoid n) => (a -> n) -> [a] -> n
+compute f = mconcat . map f 
+
+fuse :: (a -> b -> c) -> (a -> b) -> a -> c
+fuse f g x = f x (g x)
