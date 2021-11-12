024a70894275aaead0cf4716e48b9cfb66f1607b
adkamin <minichovaa@gmail.com>
2021-11-12 11:43:12 +0100

    First version ProgramDerivation.hs

--- week8/ProgramDerivation.hs
+++ week8/ProgramDerivation.hs
@@ -1,5 +1,7 @@
 module Expression where
 
+import Data.Time ( diffUTCTime, getCurrentTime )
+
 data Tree a = Leaf | Node a (Tree a) (Tree a)
   deriving (Show)
 
@@ -20,11 +22,11 @@ Case 1: t = Leaf
 
   inorderCat Leaf xs
 = { specification of inorderCat }
-  inorder Leaf ++ []
+  inorder Leaf ++ xs
 = { definition of inorder }
-  [] ++ []
+  [] ++ xs
 = { definition of (++) }
-  [] 
+  xs
   
 Case 2: t = (Node x t1 t2)
 IH1: inorderCat t1 xs = inorder t1 ++ xs
@@ -42,14 +44,65 @@ Thus, we have the following definition:
 -}
 
 inorderCat :: Tree a -> [a] -> [a]
-inorderCat Leaf _            = []
+inorderCat Leaf xs           = xs
 inorderCat (Node x t1 t2) xs = inorderCat t1 [x] ++ inorderCat t2 xs
 
 inorder' :: Tree a -> [a]
 inorder' t = inorderCat t []
 
--- TODO: make me more efficient, too
+-- 8.7.2
+{-
+After testing the run time of both, it turns out that new inorder is actually not more efficient
+-}
+
+main :: IO ()
+main = do start <- getCurrentTime
+          print $ inorder' (skewed 1000)
+          stop <- getCurrentTime
+          print $ diffUTCTime stop start
+          start1 <- getCurrentTime
+          print $ inorder (skewed 1000)
+          stop1 <- getCurrentTime
+          print $ diffUTCTime stop1 start1
+
+
+-- 8.7.3
 elems :: Tree a -> [a]
 elems Leaf = []
 elems (Node x lt rt) = x : elems lt ++ elems rt
+
+{-
+We assume elemsCat t xs = elems t ++ xs
+
+Case 1: t = Leaf
+
+  elemsCat Leaf xs
+= { above specification }
+  elems Leaf ++ xs
+= { definition of elems }
+  [] ++ xs
+= { definition of (++) }
+  xs
+
+Case 2: t = (Node x t1 t2)
+IH1: elemsCat t1 xs = elems t1 ++ xs
+IH2: elemsCat t2 xs = elems t2 ++ xs
+
+  elemsCat (Node x t1 t2) xs
+= { above specification }
+  elems (Node x t1 t2) ++ xs
+= { definition of elems }
+  x : elems t1 ++ elems t2 ++ xs
+= { IH1 and IH2 }
+  x : elemsCat t1 [] ++ elemsCat t2 xs
+
+Thus, we have the following definition:
+-}
+
+elemsCat :: Tree a -> [a] -> [a]
+elemsCat Leaf xs           = xs
+elemsCat (Node x t1 t2) xs = x : elemsCat t1 [] ++ elemsCat t2 xs
+
+elems' :: Tree a -> [a]
+elems' t = elemsCat t []
 
