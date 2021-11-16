0099d6f706d631577a6172922b4ab371acb0fbc8
adkamin <minichovaa@gmail.com>
2021-11-16 15:16:43 +0100

    PoluTypes.hs

--- week9/PolyTypes.hs
+++ week9/PolyTypes.hs
@@ -1,7 +1,10 @@
 module PolyTypes where
 
+mingle :: [a] -> [a] -> [a]
 mingle xs ys = [ e | (x,y) <- zip xs ys, e <- [x,y] ]
 
+sumWith :: (Num a) => (a -> a) -> [a] -> a
 sumWith g xs = foldr (+) 0 (map g xs)
 
+transform :: (a -> [b]) -> [a] -> [b]  -- concat takes list of lists as an argument
 transform f  = concat . map f
