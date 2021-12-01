d198cd5f18c2113a7c6fb12b5a301332e9bb60c6
adkamin <minichovaa@gmail.com>
2021-12-01 02:00:19 +0100

    Some FindDefs.hs

--- week11/FindDefs.hs
+++ week11/FindDefs.hs
@@ -1,9 +1,13 @@
 module FindDefs where
 
--- (?$) :: Maybe (a -> b) -> Maybe a -> Maybe b
+(?$) :: Maybe (a -> b) -> Maybe a -> Maybe b
+(?$) Nothing _ = Nothing
+(?$) _ Nothing = Nothing
+(?$) (Just f) (Just a) = Just (f a)
 
---product :: (Applicative t) => t a -> t b -> t (a,b)
+-- product :: (Applicative t) => t a -> t b -> t (a,b)
 
---apply :: [a -> b] -> a -> [b]
+apply :: [a -> b] -> a -> [b]
+apply (f:fs) x = f x : apply fs x
 
---apply2nd :: [a -> b -> c] -> b -> [a -> c]
+-- apply2nd :: [a -> b -> c] -> b -> [a -> c]
