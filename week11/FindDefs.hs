4789ed3e7aee2637de0ac5e970842befdaebbac3
adkamin <minichovaa@gmail.com>
2021-12-03 11:07:59 +0100

    FindDefs.hs done

--- week11/FindDefs.hs
+++ week11/FindDefs.hs
@@ -6,10 +6,10 @@ module FindDefs where
 (?$) (Just f) (Just a) = Just (f a)
 
 product :: (Applicative t) => t a -> t b -> t (a,b)
-product ta tb = (,) <$> ta <*> tb
+product ta tb = pure (,) <*> ta <*> tb
 
 apply :: [a -> b] -> a -> [b]
 apply (f:fs) x = f x : apply fs x
 
--- apply2nd :: [a -> b -> c] -> b -> [a -> c]
--- apply2nd (f:fs) b = (\c' -> ((\c -> f c) b) c') : apply2nd fs b
+apply2nd :: [a -> b -> c] -> b -> [a -> c]
+apply2nd fs x = [\y -> f y x | f <- fs]
