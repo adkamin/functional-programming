af3af21880350d3cc1d55cfe6cf5867e0c6b23ae
adkamin <minichovaa@gmail.com>
2021-12-03 09:42:08 +0100

    Result.hs

--- week11/Result.hs
+++ week11/Result.hs
@@ -5,8 +5,17 @@ import Data.List
 data Result a = Okay a | Error [String]
   deriving (Eq,Ord,Show)
 
---instance Functor Result where
---  ...
+instance Functor Result where
+  --fmap :: (a -> b) -> Result a -> Result b
+  fmap f (Error s) = Error s
+  fmap f (Okay a)  = Okay (f a)
 
---instance Applicative Result where
---  ...
+instance Applicative Result where
+  --pure :: a -> Result a
+  pure = Okay
+
+  --(<*>) :: f (a -> b) -> f a -> f b
+  (Error f) <*> (Error a) = Error (f ++ a)
+  (Error f) <*> _         = Error f
+  _         <*> (Error a) = Error a
+  (Okay f)  <*> (Okay a)  = Okay (f a)
