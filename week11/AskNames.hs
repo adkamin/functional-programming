55d910a4177590c34740b4f8f4162452144f3097
adkamin <minichovaa@gmail.com>
2021-12-03 11:49:45 +0100

    Done

--- week11/AskNames.hs
+++ week11/AskNames.hs
@@ -1,13 +1,17 @@
 module AskNames where
 
 generateNames :: [String] -> [String] -> [String]
-generateNames firstnames surnames = [ f ++ " " ++ l | f <- firstnames, l <- surnames ]
+-- generateNames firstnames surnames = [ f ++ " " ++ l | f <- firstnames, l <- surnames ]
+-- generateNames firstnames surnames = (\x y -> x ++ " " ++ y) <$> firstnames <*> surnames
+generateNames = makeName
 
 getFullName :: IO String
-getFullName = do
-  first   <- putStrLn "First name?" >> getLine
-  surname <- putStrLn "Last name?"  >> getLine
-  return (first ++ " " ++ surname)
+-- getFullName = do
+--   first   <- putStrLn "First name?" >> getLine
+--   surname <- putStrLn "Last name?"  >> getLine
+--   return (first ++ " " ++ surname)
+-- getFullName = (\x y -> x ++ " " ++ y) <$> (putStrLn "First name?" >> getLine) <*> (putStrLn "Last name?"  >> getLine)
+getFullName = makeName (putStrLn "First name?" >> getLine) (putStrLn "Last name?"  >> getLine)
 
 makeName :: (Applicative f) => f String -> f String -> f String
-makeName first surname = pure (\f l->f ++ " " ++ l) <*> first <*> surname
+makeName first surname = pure (\f l -> f ++ " " ++ l) <*> first <*> surname
