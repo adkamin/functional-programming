51e46d652a70661b6207b1fa5782a45726201d90
adkamin <minichovaa@gmail.com>
2021-12-07 13:02:29 +0100

    Notation.hs

--- week12/Notation.hs
+++ week12/Notation.hs
@@ -3,14 +3,25 @@ module Notation where
 import Data.Time
 
 siri :: IO ()
-siri = 
-  putStrLn "What is your name?" >>
-  getLine >>= \name ->
-  getZonedTime >>= \now ->
+-- siri = 
+--   putStrLn "What is your name?" >>
+--   getLine >>= \name ->
+--   getZonedTime >>= \now ->
+--   putStrLn (name ++ formatTime defaultTimeLocale ", the time is %H:%M" now)
+siri = do
+  putStrLn "What is your name?"
+  name <- getLine
+  now <- getZonedTime
   putStrLn (name ++ formatTime defaultTimeLocale ", the time is %H:%M" now)
 
+-- Looks up a key in dictionary and if found, returns its value as a Just, if not found, returns Nothing
 mayLookup :: (Eq a) => Maybe a -> [(a, b)] -> Maybe b
-mayLookup maybekey assocs = do
-  key <- maybekey
-  result <- lookup key assocs
+-- mayLookup maybekey assocs = do
+--   key <- maybekey
+--   result <- lookup key assocs
+--   return result
+mayLookup maybekey assocs = 
+  maybekey >>= \key ->
+  lookup key assocs >>= \result ->
   return result
+  
