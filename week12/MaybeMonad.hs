56ed6aa8cf0ffbcf05808ae747735c74d1924dd6
adkamin <minichovaa@gmail.com>
2021-12-07 12:50:37 +0100

    MaybeMonad.hs more

--- week12/MaybeMonad.hs
+++ week12/MaybeMonad.hs
@@ -1,16 +1,22 @@
 module MaybeMonad where
 
+import Control.Monad
+
 maybeMap :: (a -> b) -> Maybe a -> Maybe b
 maybeMap = fmap
 -- maybeMap f Nothing  = Nothing
 -- maybeMap f (Just x) = pure (f x)
 
 stripMaybe :: Maybe (Maybe a) -> Maybe a
+stripMaybe = join
+-- stripMaybe mmx = do { mx <- mmx ; mx}
+
 -- stripMaybe Nothing     = Nothing
 -- stripMaybe (Just x)    = x
-stripMaybe mmx = do { mx <- mmx ; mx}
 
 applyMaybe:: (a -> Maybe b) -> Maybe a -> Maybe b
-applyMaybe f ma = do { a <- ma ; f a}
+applyMaybe f ma = ma >>= f
+-- applyMaybe f ma = do { a <- ma ; f a}
+
 -- applyMaybe f Nothing  = Nothing
 -- applyMaybe f (Just x) = f x
